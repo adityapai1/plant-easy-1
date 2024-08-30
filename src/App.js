93006dc9b187e3af778eb19cbaed00c200b52370
Awani Khodwe <awani.khodwe@spit.ac.in>
2024-08-30 11:15:30 +0530

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Info from "./pages/Information";
 import QnA from "./pages/QnA";
